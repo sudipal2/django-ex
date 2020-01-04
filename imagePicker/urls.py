ef002f002d518204df9e33fba133904eece3eb6f
sudipal2 <55341569+sudipal2@users.noreply.github.com>
2020-01-05 00:56:14 +0530

    Add files via upload

--- imagePicker/urls.py
+++ imagePicker/urls.py
@@ -6,5 +6,5 @@ print "Step 1 from url.py"
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^get-image-for-item-id/(?P<item_id>[0-9]+)/$', views.fetchimage, name='fetchimage'),
-    url(r'^get-all-image/', views.fetchallimages, name='fetchallimages')
+    url(r'^get-all-image/$', views.fetchallimages, name='fetchallimages')
 ]
