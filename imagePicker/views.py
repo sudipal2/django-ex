ec07d3eebb9426399246edcf720b5582ecea8b5a
sudipal2 <55341569+sudipal2@users.noreply.github.com>
2020-01-05 19:50:17 +0530

    Update views.py

--- imagePicker/views.py
+++ imagePicker/views.py
@@ -30,7 +30,8 @@ def fetchallimages(request):
             i += 1
             img_ct += 1
             #img_data = get_all_items(bucket_name=bucket_name)
-            img_name = str(img_ct) + '.jpg'
+            #img_name = str(img_ct) + '.jpg'
+            img_name = "sample image"
             print("IMAGE NAME: ".img_name)
             #img_data = get_item(bucket_name=bucket_name, item_name=img_name)
             if img_data:
