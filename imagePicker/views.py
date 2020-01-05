2ff774c5125269b22769eec153b4426d40674dde
sudipal2 <55341569+sudipal2@users.noreply.github.com>
2020-01-05 18:17:06 +0530

    Update views.py

--- imagePicker/views.py
+++ imagePicker/views.py
@@ -24,11 +24,17 @@ def fetchallimages(request):
     bucket_name = "gamification-cos-standard-tkq"
     try:
 	#print('Trying to find ALL images')
-        img_data = get_all_items(bucket_name=bucket_name)
-        if img_data:
-            return HttpResponse(img_data, content_type="image/jpg")
-        else:
-            return HttpResponse('image not found')
+	i = 1
+	img_ct = 1000
+	while i < 6
+	    i += 1
+	    img_ct += 1
+            #img_data = get_all_items(bucket_name=bucket_name)
+	    img_data = get_item(bucket_name=bucket_name, img_ct)
+            if img_data:
+                return HttpResponse(img_data, content_type="image/jpg")
+            else:
+                return HttpResponse('image not found')
             
     except Exception as e:
         return HttpResponse(e)
